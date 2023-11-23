8263efc63b1f83964b42d95003b751a7f7f185f0
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2023-11-23 22:43:02 +0900

    feat: refactor file deletion logic in Utils.getEndPointResponses()

--- src/generate-types/utils.ts
+++ src/generate-types/utils.ts
@@ -100,6 +100,7 @@ export const Utils = {
   getEndPointResponses(debugOutputDirectory: string): Result[] {
     const logger = Logger.configure('Utils:getEndPointResponses')
     const results = []
+    const deleteFiles: string[] = []
     for (const type of this.getDirectories(debugOutputDirectory)) {
       for (const name of this.getDirectories(debugOutputDirectory, [type])) {
         for (const method of this.getDirectories(debugOutputDirectory, [
@@ -129,12 +130,7 @@ export const Utils = {
               const unixtime = Number.parseInt(file.split('.')[0])
               return now - unixtime > 1000 * 60 * 60 * 24 * 30
             })
-            for (const deleteFile of deleteFiles) {
-              logger.info(
-                `🚮 Delete: ${deleteFile.replace(debugOutputDirectory, '')}`
-              )
-              fs.unlinkSync(deleteFile)
-            }
+            deleteFiles.push(...deleteFiles)
 
             results.push({
               type,
@@ -147,6 +143,27 @@ export const Utils = {
         }
       }
     }
+
+    const startTime = Date.now()
+    let deleteFilesCount = 0
+    for (const deleteFile of deleteFiles) {
+      deleteFilesCount++
+
+      fs.unlinkSync(deleteFile)
+      if (deleteFilesCount % 100 === 0) {
+        const deletedPath = deleteFile.replace(debugOutputDirectory, '')
+        const elapsedTime = Date.now() - startTime
+        const estimatedTime =
+          (elapsedTime / deleteFilesCount) *
+          (deleteFiles.length - deleteFilesCount)
+        const formattedEstimatedTime = this.formatSeconds(estimatedTime)
+        logger.info(
+          `🚮 ${deleteFilesCount} files deleted (Just deleted: ${deletedPath}) ETA: ${formattedEstimatedTime}`
+        )
+      }
+    }
+    logger.info(`🚮 ${deleteFilesCount} files deleted`)
+
     return results
   },
 
@@ -247,5 +264,12 @@ export const Utils = {
       compileOptions.bannerComment = `${compileOptions.bannerComment}\n\n/** ${tsDocument} */`
     }
     return compileOptions
+  },
+
+  formatSeconds(seconds: number): string {
+    const hours = Math.floor(seconds / 3600)
+    const minutes = Math.floor((seconds - hours * 3600) / 60)
+    const _seconds = Math.floor(seconds - hours * 3600 - minutes * 60)
+    return `${hours}h ${minutes}m ${_seconds}s`
   },
 }
