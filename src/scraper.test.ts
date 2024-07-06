dca430a39e927f160431ba054993f56af739e900
Tomachi <8929706+book000@users.noreply.github.com>
2024-07-06 20:45:49 +0900

    feat: メールアドレス入力エラーへの対応とテスト時のプロキシサポート (#933)
    * feat: メールアドレス入力エラーへの対応
    
    * test: add TWITTER_EMAIL_ADDRESS env
    
    * test: add PROXY_SERVER, PROXY_USERNAME, PROXY_PASSWORD

--- src/scraper.test.ts
+++ src/scraper.test.ts
@@ -8,17 +8,32 @@ describe('Scraper', () => {
   test('login', async () => {
     const username = process.env.TWITTER_USERNAME
     const password = process.env.TWITTER_PASSWORD
-    const otpSecret = process.env.TWITTER_OTP_SECRET
-    if (!username || !password || !otpSecret) {
+    if (!username || !password) {
       throw new Error('Failed to get environment variables.')
     }
+
+    const otpSecret = process.env.TWITTER_OTP_SECRET
+    const emailAddress = process.env.TWITTER_EMAIL_ADDRESS
+
+    const proxyServer = process.env.PROXY_SERVER
+    const proxyUsername = process.env.PROXY_USERNAME
+    const proxyPassword = process.env.PROXY_PASSWORD
+    const proxyConfiguration = proxyServer
+      ? {
+          server: proxyServer,
+          username: proxyUsername,
+          password: proxyPassword,
+        }
+      : undefined
 
     scraper = new TwitterScraper({
       username,
       password,
       otpSecret,
+      emailAddress,
       puppeteerOptions: {
         userDataDirectory: './data/userdata',
+        proxy: proxyConfiguration,
       },
     })
     await expect(scraper.login()).resolves.not.toThrow()
--- src/twitter.test.ts
+++ src/twitter.test.ts
@@ -10,17 +10,32 @@ describe('Twitter', () => {
   test('login', async () => {
     const username = process.env.TWITTER_USERNAME
     const password = process.env.TWITTER_PASSWORD
-    const otpSecret = process.env.TWITTER_OTP_SECRET
-    if (!username || !password || !otpSecret) {
+    if (!username || !password) {
       throw new Error('Failed to get environment variables.')
     }
+
+    const otpSecret = process.env.TWITTER_OTP_SECRET
+    const emailAddress = process.env.TWITTER_EMAIL_ADDRESS
+
+    const proxyServer = process.env.PROXY_SERVER
+    const proxyUsername = process.env.PROXY_USERNAME
+    const proxyPassword = process.env.PROXY_PASSWORD
+    const proxyConfiguration = proxyServer
+      ? {
+          server: proxyServer,
+          username: proxyUsername,
+          password: proxyPassword,
+        }
+      : undefined
 
     twitter = await Twitter.login({
       username,
       password,
       otpSecret,
+      emailAddress,
       puppeteerOptions: {
         userDataDirectory: './data/userdata',
+        proxy: proxyConfiguration,
       },
       debugOptions: {
         outputResponse: {
