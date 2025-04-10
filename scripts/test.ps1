3aa2a87f32d9b8b4e927c24cdda931a8dbfe53cc
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2025-04-10 22:12:12 +0900

    fix: delete jest.jestCommandLine

--- scripts/test.ps1
+++
@@ -1,9 +0,0 @@
-get-content .env | ForEach-Object {
-  $name, $value = $_.split('=', 2)
-  if ([string]::IsNullOrWhiteSpace($name) -or $name.Contains('#')) {
-    return
-  }
-  set-content env:\$name $value
-}
-
-pnpm run test -- $args
